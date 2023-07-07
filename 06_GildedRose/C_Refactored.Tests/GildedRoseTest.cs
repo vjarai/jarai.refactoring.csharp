f040aa9bc349a3275e4d968ddcdd8e3cc3ab25de
Viktor <viktor@nospam.org>
2023-07-07 21:26:12 +0200

    Gildedrose Refactoring Projekte angelegt

---
+++ 06_GildedRose/C_Refactored.Tests/GildedRoseTest.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using GildedRoseKata;
+
+namespace GildedRoseTests
+{
+    public class GildedRoseTest
+    {
+        [Fact]
+        public void AgedBrie()
+        {
+            // Arrange
+            var items = new List<Item>
+            {
+                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+            };
+
+            var app = new GildedRose(items);
+
+            // ACT
+            app.UpdateQuality();
+
+
+            // ASSERT
+            Assert.Equal("Aged Brie", items[0].Name);
+        }
+    }
+}
