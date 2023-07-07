686d50f8b3644b2d935e1db6d0c6e606492e4e84
Viktor <viktor@nospam.org>
2023-07-07 22:37:35 +0200

    .

--- 06_GildedRose/C_Refactored/ProductItem.cs
+++ 06_GildedRose/C_Refactored/ProductItem.cs
@@ -4,6 +4,17 @@ namespace GildedRoseKata.Refactored
 {
     public class SellableItem : Item
     {
+
+        public new int Quality
+        {
+            get { return base.Quality; }
+            set
+            {
+                if (value >= 0 && value <= 80)
+                    base.Quality = value;
+            }
+        }
+
         public void Update()
         {
             throw new NotImplementedException();
--- 06_GildedRose/C_Refactored/Program.cs
+++ 06_GildedRose/C_Refactored/Program.cs
@@ -10,31 +10,31 @@ namespace GildedRoseKata.Refactored
             Console.WriteLine("OMGHAI!");
 
             IList<Item> items = new List<Item>{
-                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
-                new Item
+                new SellableItem {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                new SellableItem {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                new SellableItem {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                new SellableItem {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                new SellableItem {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
+                new SellableItem
                 {
                     Name = "Backstage passes to a TAFKAL80ETC concert",
                     SellIn = 15,
                     Quality = 20
                 },
-                new Item
+                new SellableItem
                 {
                     Name = "Backstage passes to a TAFKAL80ETC concert",
                     SellIn = 10,
                     Quality = 49
                 },
-                new Item
+                new SellableItem
                 {
                     Name = "Backstage passes to a TAFKAL80ETC concert",
                     SellIn = 5,
                     Quality = 49
                 },
 				// this conjured item does not work properly yet
-				new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+				new SellableItem {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
             };
 
             var app = new GildedRose(items);
