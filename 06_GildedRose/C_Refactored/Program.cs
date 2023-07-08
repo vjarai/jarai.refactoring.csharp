1c7bab67cdb02b6af101d68353bc65ae25e65874
Viktor <viktor@nospam.org>
2023-07-08 12:35:26 +0200

    .

--- 06_GildedRose/C_Refactored/Program.cs
+++ 06_GildedRose/C_Refactored/Program.cs
@@ -11,31 +11,31 @@ public class Program
 
         IList<Item> items = new List<Item>
         {
-            new SellableItem { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 },
-            new SellableItem { Name = "Aged Brie", SellIn = 2, Quality = 0 },
-            new SellableItem { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7 },
-            new SellableItem { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 },
-            new SellableItem { Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80 },
-            new SellableItem
+            new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 },
+            new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 },
+            new Item { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7 },
+            new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 },
+            new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80 },
+            new Item
             {
                 Name = "Backstage passes to a TAFKAL80ETC concert",
                 SellIn = 15,
                 Quality = 20
             },
-            new SellableItem
+            new Item
             {
                 Name = "Backstage passes to a TAFKAL80ETC concert",
                 SellIn = 10,
                 Quality = 49
             },
-            new SellableItem
+            new Item
             {
                 Name = "Backstage passes to a TAFKAL80ETC concert",
                 SellIn = 5,
                 Quality = 49
             },
             // this conjured item does not work properly yet
-            new SellableItem { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6 }
+            new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6 }
         };
 
         var app = new GildedRose(items);
