5130b544ff89cc84a37cd1c0a8bce70a2c434bd6
Viktor <viktor@nospam.org>
2023-07-08 11:16:50 +0200

    Aged Brie Rule ok

--- 06_GildedRose/C_Refactored/GildedRose.cs
+++ 06_GildedRose/C_Refactored/GildedRose.cs
@@ -1,74 +1,80 @@
 ﻿using System.Collections.Generic;
+using GildedRoseKata.Refactored.Rules;
 
 namespace GildedRoseKata.Refactored
 {
     public class GildedRose
     {
-        private const string AgedBrie = "Aged Brie";
-        private const string BackstagePassesToATafkal80EtcConcert = "Backstage passes to a TAFKAL80ETC concert";
-        private const string SulfurasHandOfRagnaros = "Sulfuras, Hand of Ragnaros";
 
         readonly IList<Item> _items;
+        private readonly UpdateService _updateService;
+
         public GildedRose(IList<Item> items)
         {
             _items = items;
+            _updateService = new UpdateService();
         }
 
         public void UpdateQuality()
         {
             foreach (var item in _items)
             {
-                if (item.Name == AgedBrie || item.Name == BackstagePassesToATafkal80EtcConcert)
+                if (item.Name == UpdateService.AgedBrie)
+                {
+                    _updateService.UpdateItem(item);
+                    continue;
+                }
+
+
+                if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
                 {
                     if (item.Quality < 50)
                     {
                         item.Quality += 1;
 
-                        if (item.Name == BackstagePassesToATafkal80EtcConcert)
+                        if (item.SellIn < 11)
                         {
-                            if (item.SellIn < 11)
+                            if (item.Quality < 50)
                             {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality += 1;
-                                }
-                            }
-
-                            if (item.SellIn < 6)
-                            {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality += 1;
-                                }
+                                item.Quality += 1;
                             }
                         }
+
+                        if (item.SellIn < 6)
+                        {
+                            if (item.Quality < 50)
+                            {
+                                item.Quality += 1;
+                            }
+                        }
+
                     }
                 }
                 else
                 {
                     if (item.Quality > 0)
                     {
-                        if (item.Name != SulfurasHandOfRagnaros)
+                        if (item.Name != "Sulfuras, Hand of Ragnaros")
                         {
                             item.Quality -= 1;
                         }
                     }
                 }
 
-                if (item.Name != SulfurasHandOfRagnaros)
+                if (item.Name != "Sulfuras, Hand of Ragnaros")
                 {
                     item.SellIn -= 1;
                 }
 
                 if (item.SellIn < 0)
                 {
-                    if (item.Name != AgedBrie)
+                   
                     {
-                        if (item.Name != BackstagePassesToATafkal80EtcConcert)
+                        if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
                         {
                             if (item.Quality > 0)
                             {
-                                if (item.Name != SulfurasHandOfRagnaros)
+                                if (item.Name != "Sulfuras, Hand of Ragnaros")
                                 {
                                     item.Quality -= 1;
                                 }
@@ -77,13 +83,6 @@ namespace GildedRoseKata.Refactored
                         else
                         {
                             item.Quality -= item.Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (item.Quality < 50)
-                        {
-                            item.Quality += 1;
                         }
                     }
                 }
---
+++ 06_GildedRose/C_Refactored/Rules/UpdateService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GildedRoseKata.Refactored.Rules
+{
+    public class UpdateService
+    {
+        private Dictionary<string, UpdateRule> _rules;
+
+        public const string AgedBrie = "Aged Brie";
+
+        public const string BackstagePassesToATafkal80EtcConcert = "Backstage passes to a TAFKAL80ETC concert";
+
+        public const string SulfurasHandOfRagnaros = "Sulfuras, Hand of Ragnaros";
+
+        public UpdateService()
+        {
+            _rules = new Dictionary<string, UpdateRule>()
+            {
+                { AgedBrie, new UpdateRule(){QualityChangePerDay = 1} },
+                { SulfurasHandOfRagnaros, new UpdateRule{QualityChangePerDay = 0, MinQuality = 80, MaxQuality = 80} }
+            };
+        }
+
+        public void UpdateItem(Item itemToUpdate)
+        {
+            var rule = _rules[itemToUpdate.Name];
+            rule.Update(itemToUpdate);
+        }
+    }
+}
